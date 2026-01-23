99aba1a226728a4d5f260f3f8394af0034e6e943
Vladimir Demenev <vademenev@gmail.com>
2026-01-23 12:24:32 +0300

    Fixed a bug with output filenames

--- comfyui-saveimage-extra/save_image_extra.py
+++ comfyui-saveimage-extra/save_image_extra.py
@@ -72,15 +72,14 @@ class SaveImageExtraNode:
         self.compress_level = 4
 
     def save_images(self, images, filename_prefix="ComfyUI", extradata="", prompt=None, extra_pnginfo=None):
-        
         filename_prefix += self.prefix_append
-        
+
         full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(
             filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0]
         )
         
         results = list()
-
+        
         for (batch_number, image) in enumerate(images):
             i = 255. * image.cpu().numpy()
             img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
@@ -115,7 +114,7 @@ class SaveImageExtraNode:
                         metadata.add_text('extradata', extradata)
 
             filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
-            file = f"{filename_with_batch_num}_{counter:05}.png"
+            file = f"{filename_with_batch_num}_{counter:05}_.png"
 
             img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
 
